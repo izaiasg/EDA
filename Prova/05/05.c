3d4e17b9c28453ca71467929f6f0743eb6bfdbac
izaiasg <izaias.rogerio96@gmail.com>
2018-12-13 00:51:28 -0200

    Update 05.c

--- Prova/05/05.c
+++ Prova/05/05.c
@@ -8,163 +8,258 @@ struct No{
 	struct No *prox;
 };
 
-// Protótipos das funções
-void init(struct No grafo[n]);
-void inserirLigacao(int vertice1, int vertice2, struct No grafo[n]);
-void inserir(int numero, struct No **fila);
-void snapshot(struct No grafo[n]);
-void printar(struct No **fila);
-void buscaProfundidade(struct No grafo[n], int inicial, int *visitado);
-void buscaProfundidadeAux(struct No **fila, int inicial, int *visitado, int cont);
+//Protótipos das funções
+void imprimeFila(struct No *fila, const int base[14]);
+void imprimePilha(struct No *pilha, const int base[14]);
+int buscaFila(struct No *fila, int valor);
+int primeiroDaFila(struct No *fila);
+void desenfileirar(struct No **fila);
+void enfileirar(struct No **fila, int valor);
+struct No *buscaLargura(const int grafoMatriz[14][14], int tamanho,int verticeInicial);
+struct No *buscaProfundidade(const int grafoMatriz[14][14], int tamanho,int verticeInicial);
+void empilhar(struct No **pilha, int valor);
+int primeiroDaPilha(struct No *pilha);
+int buscaPilha(struct No *pilha, int valor);
+void desempilhar(struct No **pilha);
+
+
 
 
 // Função main
-int main(int argc, char const *argv[]){
-	
-	struct No grafo[n];
-    int visitado[n];
-    int vertceInicial = 1;
+int main(int argc, char *argv[]){
 
-	init(grafo);
+    int verticeInicial = 1;
 
-	inserirLigacao(1,2,grafo);
-    inserirLigacao(1,3,grafo);
-    inserirLigacao(1,7,grafo);
-    inserirLigacao(2,1,grafo);
-    inserirLigacao(2,3,grafo);
-    inserirLigacao(3,1,grafo);
-    inserirLigacao(3,2,grafo);
-    inserirLigacao(3,4,grafo);
-    inserirLigacao(3,5,grafo);
-    inserirLigacao(4,3,grafo);
-    inserirLigacao(4,5,grafo);
-    inserirLigacao(5,3,grafo);
-    inserirLigacao(5,4,grafo);
-    inserirLigacao(5,6,grafo);
-    inserirLigacao(6,5,grafo);
-    inserirLigacao(6,8,grafo);
-    inserirLigacao(6,9,grafo);
-    inserirLigacao(7,1,grafo);
-    inserirLigacao(7,8,grafo);
-    inserirLigacao(8,7,grafo);
-    inserirLigacao(8,6,grafo);
-    inserirLigacao(8,9,grafo);
-    inserirLigacao(9,6,grafo);
-    inserirLigacao(9,8,grafo);
-    inserirLigacao(9,10,grafo);
-    inserirLigacao(9,13,grafo);
-    inserirLigacao(10,9,grafo);
-    inserirLigacao(10,11,grafo);
-    inserirLigacao(10,13,grafo);
-    inserirLigacao(11,10,grafo);
-    inserirLigacao(11,12,grafo);
-    inserirLigacao(12,11,grafo);
-    inserirLigacao(12,13,grafo);
-    inserirLigacao(13,9,grafo);
-    inserirLigacao(13,10,grafo);
-    inserirLigacao(13,12,grafo);
+    int base[n];
 
-	snapshot(grafo);
+    // Preencendo o array que servirá como base
+    for(int i=0; i<n; i++){
+        base[i] = i;
+    }
+    // Prenchendo a matriz com de acordo com a imagem da questão
+                                    /*  0 1 2 3 4 5 6 7 8 9 x z w q */
+    int grafoMatriz[n][n]= {0,0,0,0,0,0,0,0,0,0,0,0,0,0, // 0
+					  			        0,0,1,1,0,0,0,1,0,0,0,0,0,0, // 1
+					  			        0,1,0,1,0,0,0,0,0,0,0,0,0,0, // 2
+                                        0,1,1,0,1,1,0,0,0,0,0,0,0,0, // 3
+                                        0,0,0,1,0,1,0,0,0,0,0,0,0,0, // 4
+                                        0,0,0,1,1,0,1,0,0,0,0,0,0,0, // 5
+                                        0,0,0,0,0,1,0,0,1,1,0,0,0,0, // 6
+                                        0,1,0,0,0,0,0,0,1,0,0,0,0,0, // 7
+                                        0,0,0,0,0,0,1,1,0,1,0,0,0,0, // 8
+                                        0,0,0,0,0,0,1,0,1,0,1,0,0,1, // 9
+                                        0,0,0,0,0,0,0,0,0,1,0,1,0,1, // 10
+                                        0,0,0,0,0,0,0,0,0,0,1,0,1,0, // 11
+                                        0,0,0,0,0,0,0,0,0,0,0,1,0,1, // 12
+                                        0,0,0,0,0,0,0,0,0,1,1,0,1,0};// 13
 
+    // Imprimindo a busca em largura
+    printf("Resultado da busca em largura: ");
+    imprimeFila(buscaLargura(grafoMatriz,n,verticeInicial), base);
 
-    buscaProfundidade(grafo, vertceInicial, visitado);
-	
+    // Imprimindo a busca em profundidade
+    printf("\nResultado da busca em profundidade: ");
+    imprimePilha(buscaProfundidade(grafoMatriz,n,verticeInicial),base);
+
+    return 0;
+
+}
+
+// Função que imprime a fila da busca em largura
+void imprimeFila(struct No *fila, const int base[14]){
+
+	if (fila == NULL){
+		return;
+	}
+
+	if (fila->prox){
+		imprimeFila(fila->prox,base);
+	}
+
+	printf("%d ", base[fila->valor]);
+}
+
+// Função busca em largura
+struct No *buscaLargura(const int grafoMatriz[14][14], int tamanho,int verticeInicial){
+
+	// Declarando as filas
+	struct No *fila = NULL; 
+	struct No *marcado = NULL; // Irá conter o caminho percorrido na busca em largura
+
+	// Lendo os dados 
+	enfileirar(&fila,verticeInicial);
+	enfileirar(&marcado,verticeInicial);
+
+	for(int j = 0; fila; ++j){
+		int primeiroFila = primeiroDaFila(fila);
+
+		for(int i = 0; i < tamanho; ++i){
+
+			if(grafoMatriz[primeiroFila][i] == 1 && buscaFila(marcado, i) == 0){
+				enfileirar(&fila,i);
+				enfileirar(&marcado,i);
+			}
+		}
+
+		desenfileirar(&fila);
+	}
+
+	return marcado;
+
+}
+
+// Função que retorna o primeiro da fila
+int primeiroDaFila(struct No *fila){
+
+	while((fila)->prox){
+		(fila) = (fila)->prox;
+	}
+
+	return (fila)->valor;
+}
+
+// Função que cria e preenche filas (enfileirar)
+void enfileirar(struct No **fila, int valor){
+
+	if (!(*fila) ){
+		(*fila) = (struct No*)malloc(sizeof(struct No));
+		(*fila)->prox = NULL;
+		(*fila)->valor = valor;
+	}else{
+
+		struct No *aux = (struct No*)malloc(sizeof(struct No));
+		aux->valor = valor;
+		aux->prox = (*fila);
+		(*fila) = aux;
+	}
+}
+
+// Função que retira da fila (desenfileirar)
+void desenfileirar(struct No **fila){
+
+	struct No *cabeca = (*fila);
+	struct No *aux = NULL;
+
+	while((*fila)->prox){
+		aux = (*fila);
+		(*fila) = (*fila)->prox;
+	}
+
+	if ((*fila) == cabeca){
+		(*fila) = NULL;
+
+	}else{
+		aux->prox = NULL;
+		(*fila) = cabeca;
+	}
+
+}
+
+// Busca na fila 
+int buscaFila(struct No *fila, int valor){
+
+	while((fila)){
+
+		if ((fila)->valor == valor){
+			return 1;
+		}
+
+		fila = fila->prox;
+	}
+	return 0;
+}
+
+// Função que imprime a pilha da busca em profundidade
+void imprimePilha(struct No *pilha, const int base[14]){
+
+	if (pilha == NULL){
+		return;
+	}
+
+	if (pilha->prox){
+		imprimePilha(pilha->prox,base);
+	}
+
+	printf("%d ", base[pilha->valor]);
+}
+
+// Função busca em profundidade
+struct No *buscaProfundidade(const int grafoMatriz[14][14], int tamanho,int verticeInicial){
+
+	// Declarando as pilhas
+	struct No *pilha = NULL; 
+	struct No *marcado = NULL; // Irá conter o caminho percorrido na busca em largura
+
+	// Lendo os dados 
+	empilhar(&pilha,verticeInicial);
+	empilhar(&marcado,verticeInicial);
+
+	for(int j = 0; pilha; ++j){
+
+		int primeiroPilha = primeiroDaPilha(pilha);
+		desempilhar(&pilha);
+
+		for(int i = 0; i < tamanho; ++i){
+
+			if(grafoMatriz[primeiroPilha][i] == 1 && buscaPilha(marcado, i) == 0){
+				empilhar(&pilha,i);
+				empilhar(&marcado,i);
+			}
+		}
+	}
+
+	return marcado;
+}
+
+// Função que empilha os dados
+void empilhar(struct No **pilha, int valor){
+
+	if (!(*pilha)){
+		
+		(*pilha) = (struct No*)malloc(sizeof(struct No));
+		(*pilha)->prox = NULL;
+		(*pilha)->valor = valor;
+	}else{
+
+		struct No *aux = (struct No*)malloc(sizeof(struct No));
+		aux->valor = valor;
+		aux->prox = (*pilha);
+		(*pilha) = aux;
+	}
+}
+
+// Função que retorna o primeiro da pilha
+int primeiroDaPilha(struct No *pilha){
+
+	if (pilha){
+
+		return pilha->valor;
+	}
+}
+
+// Função que busca na pilha
+int buscaPilha(struct No *pilha, int valor){
+
+	while(pilha){
+
+		if (pilha->valor == valor){
+			return 1;
+		}
+
+		pilha = pilha->prox;
+	}
 
 	return 0;
 }
 
-void init(struct No grafo[n]){
-	for (int i = 0; i < n; i++){
-		grafo[i].valor = i;
-		grafo[i].prox = NULL;
-		//printf("%d\n", grafo[i]);
-		//printf("\n\n");
-	}
-}
+// Função que desempilha os dados
+void desempilhar(struct No **pilha){
 
-void inserirLigacao(int vertice1, int vertice2, struct No grafo[n]){
-	//printf("vertice1: %d :vertice2: %d\n", vertice1, vertice2);
-	inserir(vertice2, &(grafo[vertice1].prox));
-
-}
-
-
-void inserir(int numero, struct No **fila){
-
-	if((*fila) == NULL){
-		struct No *tmp = malloc(sizeof(struct No));
-		tmp->valor = numero;
-		tmp->prox = NULL;
-		(*fila) = tmp;
-	}else{
-		inserir(numero, &((*fila)->prox));
-	}
-
-}
-
-void snapshot(struct No grafo[n]){
-	for (int i = 0; i < n; ++i){
-
-		printf("%d -> ", grafo[i].valor);
-		printar(&(grafo[i].prox));
-		printf("\n");
-	}
-}
-
-void printar(struct No **fila){
-	if(*fila == NULL){
-		printf("Nao se relaciona");
-	}else{
-		struct No *tmp = *(fila);
-
-		while(tmp != NULL){
-			printf("%d ", tmp->valor);
-			tmp = tmp->prox;
-		}	
-	}
-	
-}
-
-
-// largura ---------------------------------------------------
-
-
-// profundidade ---------------------------------------------------
-
-// Função auxiliar para realizar o calculo da busca
-void buscaProfundidadeAux(struct No **fila, int inicial, int *visitado, int cont){
-    int i;
-    visitado[inicial] = cont;
-
-    // percorrer todas as arestas que partem desse vertice
-    if(*fila == NULL){
-        printf("Entrou no if e retornou");
-		return;
+	if((*pilha)->prox){
+		(*pilha) = (*pilha)->prox;
 	}else{
 
-		struct No *tmp = *(fila);
-
-		while(tmp != NULL){
-			
-            if(!visitado[tmp[i].valor]){
-                buscaProfundidadeAux(&(tmp[i].prox),tmp[i].valor,visitado,cont+1);
-            }
-            
-			tmp = tmp->prox;
-		}	
+		(*pilha) = NULL;
 	}
+
 }
-
-// Função principal que faz a interação com o usuário
-void buscaProfundidade(struct No grafo[n], int inicial, int *visitado){
-    int i, cont = 1;
-    for(i=0; i < n; i++){
-        visitado[i] = 0;
-        buscaProfundidadeAux(&(grafo[i].prox),inicial,visitado,cont);
-    }
-
-    for(i=0; i < n; i++){
-        printf("%d -> %d\n",i,visitado[i]);
-    }
-}
-
